1b1a5e50399c4ea711a9a468638da61874111e87
Asoul <b00902036@ntu.edu.tw>
2016-06-22 18:01:07 +0800

    daily update

--- run.sh
+++ run.sh
@@ -4,3 +4,6 @@ cd /home/asoul/day_volume/
 /usr/bin/git add .
 /usr/bin/git commit -m "daily update"
 /usr/bin/git push
+/usr/bin/git fetch --depth=1
+/usr/bin/git reflog expire --expire-unreachable=now --all
+/usr/bin/git gc --aggressive --prune=all
